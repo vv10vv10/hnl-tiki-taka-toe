3dbb4fbee37effb89276a4e651efa99ce1fe645c
VanjaV10 <vv55136@fer.hr>
2026-06-26 22:49:10 +0200

    Add initial Django setup and models

--- game/models.py
+++ game/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Drzava(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Klub(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Igrac(models.Model):
+    name = models.CharField(max_length=100)
+    clubs = models.ManyToManyField(Klub)
+    country = models.ForeignKey(Drzava,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
