58c90d86e0c812556ff83b8cda8ce5557b140d74
markocusa <marko.cusavuksan@gmail.com>
2026-07-22 12:25:17 +0200

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://127.0.0.1:8000/api'
+  production: true,
+  apiUrl: 'https://hnl-tiki-taka-toe-production.up.railway.app/api'
 };
